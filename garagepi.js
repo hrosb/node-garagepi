d14a919ec5046e86724a2d5820a6e45c87064dc7
Harald Rosberg <harald.rosberg@appex.no>
2019-08-11 14:17:57 +0200

    minor refactoring

--- garagepi.js
+++ garagepi.js
@@ -1,59 +1,94 @@
-
-var first_arg = process.argv[2]
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-if(first_arg === undefined){
+var first_arg = process.argv[2];
+var path = require("path");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+if (first_arg === undefined) {
   var GPIO = require("onoff").Gpio;
 }
-var express = require('express');
+var express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 var startTakingSnaps = false;
-var auth = require('./auth');
+var auth = require("./auth");
 
-
-require('console-stamp')(console, '[HH:MM:ss]');
+require("console-stamp")(console, "[HH:MM:ss]");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
-  res.render('index.html');
+app.get("/", function(req, res) {
+  res.render("index.html");
 });
 
-var state = 'closed';
-app.get('/api/clickbutton', auth.staticUserAuth, function(req, res) {
-  state = state == 'closed' ? 'open' : 'closed';
+var state = "closed";
+app.get("/api/clickbutton", auth.staticUserAuth, function(req, res) {
+  state = state == "closed" ? "open" : "closed";
 
   // hardcode to closed for now until reed switch
-  state = 'closed';
-  res.setHeader('Content-Type', 'application/json');
-  res.end('{"success" : "Updated Successfully", "status" : "'+state+'"}');
-  outputSequence(7, '10', 1000);
+  state = "closed";
+  res.setHeader("Content-Type", "application/json");
+  res.end('{"success" : "Updated Successfully", "status" : "' + state + '"}');
+  outputSequence(7, "10", 1000);
 });
 
-app.get('/api/status', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
+app.get("/api/door/:side", auth.staticUserAuth, function(req, res) {
+  
+  side = req.params.side;
+
+  state = state == "closed" ? "open" : "closed";
+
+  // hardcode to closed for now until reed switch
+  state = "closed";
+  res.setHeader("Content-Type", "application/json");
+  res.end('{"success" : "Updated Successfully", "status" : "' + state + '"}');
+  toggleDoor(side);
+});
+
+app.get("/api/status", function(req, res) {
+  res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ state: state }));
-  console.log('returning state: ' + state);
+  console.log("returning state: " + state);
 });
 
 function outputSequence(pin, seq, timeout) {
-  var gpio = new GPIO(4, 'out');
+  var gpio = new GPIO(4, "out");
   gpioWrite(gpio, pin, seq, timeout);
 }
 
+function toggleDoor(side){
+  var gpio;
+  var pin;
+  switch (side) {
+    case 'left':
+      
+      gpio = new GPIO(4, "out");
+      pin = 7;
+
+      break;
+
+    case 'right':
+    
+      gpio = new GPIO(5, "out");
+      pin = 8;
+
+      break;      
+
+  }
+
+  gpioWrite(gpio, pin, "10", 1000);
+
+}
+
 function gpioWrite(gpio, pin, seq, timeout) {
-  if (!seq || seq.length <= 0) { 
-    console.log('closing pin:', pin);
+  if (!seq || seq.length <= 0) {
+    console.log("closing pin:", pin);
     gpio.unexport();
     return;
   }
@@ -61,7 +96,7 @@ function gpioWrite(gpio, pin, seq, timeout) {
   var value = seq.substr(0, 1);
   seq = seq.substr(1);
   setTimeout(function() {
-    console.log('gpioWrite, value:', value, ' seq:', seq);
+    console.log("gpioWrite, value:", value, " seq:", seq);
     gpio.writeSync(value);
     gpioWrite(gpio, pin, seq, timeout);
   }, timeout);
@@ -69,24 +104,24 @@ function gpioWrite(gpio, pin, seq, timeout) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 function takeSnaps() {
   var autoSnapshot = setTimeout(function() {
-    var imgPath = path.join(__dirname, 'public/images');
-    var cmd = 'raspistill -w 640 -h 480 -q 80 -o ' + imgPath + '/garage.jpg';
-    var exec = require('child_process').exec;
-    exec(cmd, function (error, stdout, stderr) {
+    var imgPath = path.join(__dirname, "public/images");
+    var cmd = "raspistill -w 640 -h 480 -q 80 -o " + imgPath + "/garage.jpg";
+    var exec = require("child_process").exec;
+    exec(cmd, function(error, stdout, stderr) {
       if (error !== null) {
-        console.log('exec error: ' + error);
+        console.log("exec error: " + error);
         return;
       }
-      io.emit('snapshot', 'ready');
-      console.log('snapshot created...');
-      if(startTakingSnaps) {
+      io.emit("snapshot", "ready");
+      console.log("snapshot created...");
+      if (startTakingSnaps) {
         takeSnaps();
       }
     });
@@ -95,18 +130,18 @@ function takeSnaps() {
   return autoSnapshot;
 }
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on("connection", function(socket) {
+  console.log("a user connected");
   startTakingSnaps = true;
   takeSnaps();
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
     startTakingSnaps = false;
   });
 });
 
 var port = process.env.PORT || 8000;
 server.listen(port, function() {
-  console.log('GaragePi listening on port:', port);
+  console.log("GaragePi listening on port:", port);
 });
--- src/main.js
+++ src/main.js
@@ -1,1 +1,48 @@
-import style from './style.scss';
+import style from "./style.scss";
+
+const leftDoorBtn = document.getElementById("door-left");
+const rightDoorBtn = document.getElementById("door-right");
+const passwordForm = document.getElementById("password-form");
+
+let existingPass = localStorage.getItem("garasjepass");
+let password = "";
+
+if (existingPass) {
+  passwordForm.style.display = "none";
+}
+
+leftDoorBtn.addEventListener("click", evt => {
+  toggleDoor("left");
+});
+rightDoorBtn.addEventListener("click", evt => {
+  toggleDoor("right");
+});
+
+function toggleDoor(side) {
+  const passwordValue = document.getElementById("pass").value;
+
+  console.log(existingPass);
+  console.log(passwordValue);
+  if (existingPass) {
+    password = existingPass;
+  } else {
+    password = passwordValue;
+  }
+
+  const headers = new Headers({
+    Authorization: "Basic " + btoa("Admin" + ":" + password)
+  });
+
+  fetch("api/door/" + side, {
+    method: 'GET',
+    headers: headers
+  })
+    .then(function(response) {
+      console.log(response);
+      localStorage.setItem('garasjepass', password);
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+    });
+}
